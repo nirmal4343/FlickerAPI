63b8b0d0927fd191d78e5f5f89339e5e6eda42b7
unknown <nirmal.4343@gmail.com>
2015-11-22 12:47:30 -0500

    FlickerAPI demo, download image

--- app/src/main/java/com/demo/flickerapi/FlickerMainActivityFragment.java
+++ app/src/main/java/com/demo/flickerapi/FlickerMainActivityFragment.java
@@ -22,7 +22,6 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
-
  * <h1> Fragment   </h1>
  * This is the fragment display in MainActivity to display the downloaded image from flicker.
  * Also, has the implementation of network call using AsyncTask to pull the JSON response from server
