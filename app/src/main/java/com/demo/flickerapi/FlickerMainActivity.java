e2b35f030af4625413cb3c6a996da3ad0d22cfcd
unknown <nirmal.4343@gmail.com>
2015-11-22 22:12:30 -0500

    test, this needs to revert it back

--- app/src/main/java/com/demo/flickerapi/FlickerMainActivity.java
+++ app/src/main/java/com/demo/flickerapi/FlickerMainActivity.java
@@ -1,19 +1,21 @@
 package com.demo.flickerapi;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.graphics.Point;
-import android.graphics.Rect;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
-import com.android.volley.toolbox.NetworkImageView;
-import com.demo.flickerapi.config.AppConfig;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.demo.flickerapi.model.Photo;
-import com.demo.flickerapi.model.image.ImageCacheManager;
 
 
 
@@ -26,23 +28,137 @@ import com.demo.flickerapi.model.image.ImageCacheManager;
  * @Date 9/3/2015
  */
 
-public class FlickerMainActivity extends AppCompatActivity implements FlickerMainActivityFragment.OnHeadlineSelectedListener {
+public class FlickerMainActivity extends ActionBarActivity  implements FlickerMainActivityFragment.OnHeadlineSelectedListener {
 
-    private Animator mCurrentAnimator;
-    private int mShortAnimationDuration;
+    private ListView mDrawerList;
+
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+
+    AutoCompleteTextView text;
+
+    String[] languages={"Android ","java","IOS","SQL","JDBC","Web services"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_flicker_main);
 
+        mDrawerList = (ListView)findViewById(R.id.navList);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        mActivityTitle = getTitle().toString();
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerAccount);
+
+        final String[] accountNames = {"1010 CollingWood Ln", "1100 Atlantis Pl", "11475 Great Oaks Way"};
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, accountNames);
+
+        spinner.setAdapter(adp);
+
+        text=(AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,languages);
+
+        text.setAdapter(adapter);
+
+        text.setThreshold(1);
+
+        addDrawerItems();
+
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
     public void onPhotoSelected(View view, Photo photo) {
 
-        zoomImageFromThumb (view, photo);
+        //zoomImageFromThumb (view, photo);
     }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(FlickerMainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_fliker_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     /**
      * Zoom/Scale the selected image from the GridView cell
@@ -50,7 +166,7 @@ public class FlickerMainActivity extends AppCompatActivity implements FlickerMai
      * @param thumbView
      * @param photo
      */
-
+/*
     private void zoomImageFromThumb(final View thumbView, Photo photo) {
         // If there's an animation in progress, cancel it immediately and
         // proceed with this one.
@@ -195,5 +311,5 @@ public class FlickerMainActivity extends AppCompatActivity implements FlickerMai
                 mCurrentAnimator = set;
             }
         });
-    }
+    }*/
 }
